329faaff7d9e3ae22f13cdba550757f217f26fa8
concordance <asterartworks@gmail.com>
2022-05-17 21:03:40 +0200

    LEAFs - openzeppelin

--- contracts/governance/LEAF.sol
+++ contracts/governance/LEAF.sol
@@ -1,8 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.4;
-
 /* 
-
 @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@(/&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
@@ -36,8 +33,79 @@ pragma solidity ^0.8.4;
 @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
                                                                                 
 
-
 @title LEAF governance NFT
 @dev ERC721 contract representing the governance NFT. 
 ---------
 */
+
+pragma solidity ^0.8.4;
+
+import "../openzeppelin/token/ERC721/ERC721.sol";
+import "../openzeppelin/token/ERC721/extensions/ERC721Enumerable.sol";
+import "../openzeppelin/token/ERC721/extensions/ERC721URIStorage.sol";
+import "../openzeppelin/access/Ownable.sol";
+import "../openzeppelin/utils/Counters.sol";
+import "../openzeppelin/utils/cryptography/draft-EIP712.sol";
+import "../openzeppelin/token/ERC721/extensions/draft-ERC721Votes.sol";
+
+
+contract Leafs is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable, EIP712, ERC721Votes {
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _tokenIdCounter;
+
+    constructor() ERC721("Leafs", "LEAF") EIP712("Leafs", "1") {}
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "ipfs://";
+    }
+
+    function safeMint(address to, string memory uri) public onlyOwner {
+        uint256 tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, uri);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId)
+        internal
+        override(ERC721, ERC721Enumerable)
+    {
+        super._beforeTokenTransfer(from, to, tokenId);
+    }
+
+    function _afterTokenTransfer(address from, address to, uint256 tokenId)
+        internal
+        override(ERC721, ERC721Votes)
+    {
+        super._afterTokenTransfer(from, to, tokenId);
+    }
+
+    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+        super._burn(tokenId);
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+
+    
+
+
+}
