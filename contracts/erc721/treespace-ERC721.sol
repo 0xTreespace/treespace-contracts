a261aee371fa3c3fc12ed7294e6a2950cd6e020c
concordance <asterartworks@gmail.com>
2022-05-17 11:23:24 +0200

    added governance functions

--- contracts/erc721/treespace-ERC721.sol
+++ contracts/erc721/treespace-ERC721.sol
@@ -40,9 +40,9 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@///////////////////@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-                                      
-                                                                                                                                                            
-                                                                                
+
+@title ERC721 contract for the treespace NFTs                                                                                                                                                
+
 */
 
 /// @custom:security-contact support@treespace.xyz
@@ -53,43 +53,8 @@ contract Treespace is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable {
 
     constructor() ERC721("Treespace", "TREE") {}
 
-    // tokenID => basispointsRoyalties
-    mapping(uint => uint) public marketplaceRoyalties;
-
-    // tokenID => creator
-    mapping(uint => address) public creatorOfToken;
-
     function _baseURI() internal pure override returns (string memory) {
         return "ar://";
-    }
-
-    /*
-    @title publicMint
-    @dev allows anyone to mint an NFT to the contract
-    @param URI the IPFS hash pointing to the metadata
-    @param _royaltiesBasisPoints the Royalties to be set
-    */
-    function publicMint(string memory _URI, uint _royaltiesBasisPoints) public {
-        uint256 tokenId = _tokenIdCounter.current();
-
-        _tokenIdCounter.increment();
-        _safeMint(msg.sender, tokenId);
-        _setTokenURI(tokenId, _URI);
-
-        // set the royalties
-        _setMarketplaceRoyalties(_royaltiesBasisPoints, tokenId);
-
-        // remember the creator
-        creatorOfToken[tokenId] = msg.sender;
-    }
-
-    /* 
-    @dev get the royalties for a specific NFT - for the marketplace contracts
-    @parma _tokenID used to get the basispoint
-    @returns uint the basispoints
-    */
-    function getRoyaltiesOfToken(uint _tokenID) public view returns (uint) {
-        return(marketplaceRoyalties[_tokenID]);
     }
 
     // The following functions are overrides required by Solidity.
@@ -122,13 +87,83 @@ contract Treespace is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable {
         return super.supportsInterface(interfaceId);
     }
 
+    // ------------------------------------------------------------
+    // --------------------- Custom Functions ---------------------
+    // ------------------------------------------------------------
+
+    // tokenID => creator
+    mapping(uint => address) public creatorOfToken;
+
+    // tokenID => basispointsRoyalties
+    mapping(uint => uint) public marketplaceRoyalties;
+
+    // permissioned minting - by default true
+    // means only approved addresses can mint
+    bool public permissionedMinting = true;
+
+    // if _permissionedMinting = true, only these addresses 
+    // can call the mint function
+    mapping (address => bool) public approvedAddresses;
+
+    // max royalties - default to 1000 or 10%
+    uint _maxRoyalties = 1000;
+
+    /*
+    EVENTS
+    ---------------
+    */
+
+    event MintReceipt(string indexed _URI, uint _royaltiesBasisPoints, uint indexed _tokenID, address indexed _minter);
+    event changedMarketplaceRoyalties(uint indexed _tokenID, uint _newBasisPoints);
+
+
+    /*
+    MINT FUNCTION
+    ---------------
+        - check if mintable by anyone
+        - mint the NFT if checks passed
+
+    @title publicMint
+    @dev allows anyone to mint an NFT to the contract
+    @param URI the IPFS hash pointing to the metadata
+    @param _royaltiesBasisPoints the Royalties to be set
+    */
+    function mint(string memory _URI, uint _royaltiesBasisPoints) public {
+        if(permissionedMinting == true) {
+            // check if the address has been approved
+            require(approvedAddresses[msg.sender] == true, "ERC721::Mint:Not authorized to mint.");
+            _mintToken(_URI, _royaltiesBasisPoints);
+        } else {
+            // minting is open for all
+            _mintToken(_URI, _royaltiesBasisPoints);
+        }
+    }   
+
+    // mint a token
+    function _mintToken(string memory _URI, uint _royaltiesBasisPoints) internal {
+        uint256 tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+        _safeMint(msg.sender, tokenId);
+        _setTokenURI(tokenId, _URI);
+
+        // set the royalties
+        _setMarketplaceRoyalties(_royaltiesBasisPoints, tokenId);
+
+        // remember the creator
+        creatorOfToken[tokenId] = msg.sender;
+        
+        emit MintReceipt(_URI, _royaltiesBasisPoints, tokenId, msg.sender);
+    }
+
+
+
     /* 
     @dev set the royalties for the NFT
     @param _basispoints max. 10_000
     @param _tokenID 
     */
     function _setMarketplaceRoyalties(uint _basispoints, uint _tokenID) internal {
-        require(_basispoints <= 3000, "ERC721::Royalities must be below or equal to 30 percent!");
+        require(_basispoints <= _maxRoyalties, "ERC721::_setMarketplaceRoyalties:Royalities must be below or equal to 30 percent!");
 
         // set the royalties for the NFT
         marketplaceRoyalties[_tokenID] = _basispoints;
@@ -140,25 +175,110 @@ contract Treespace is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable {
     * can only be lowered
     * no less than zero
 
-    @check the NFT was createde by msg.sender, also makes sure the token exists
+    @check the NFT was created by msg.sender, also makes sure the token exists (msg.sender != address(0x0))
     @check basispoints cannot be zero
     @check _newBasisPoints must be lower than _currentBasisPoints
+
     @param _tokenID the ID for the token
     @param _newBasisPoints the new royalties
     */
     function changeMarketplaceRoyalties(uint _tokenID, uint _newBasisPoints) public {
-        require(msg.sender == creatorOfToken[_tokenID], "ERC721:Only creator can change Royalties.");
-        require(marketplaceRoyalties[_tokenID] != 0, "ERC721:Royalties already at 0!");
-        require(marketplaceRoyalties[_tokenID] > _newBasisPoints, "ERC721:Royalties can only be lowered!");
+        require(msg.sender == creatorOfToken[_tokenID], "ERC721::changeMarketplaceRoyalties:Only creator can change Royalties.");
+        require(marketplaceRoyalties[_tokenID] != 0, "ERC721::changeMarketplaceRoyalties:Royalties already at 0!");
+        require(marketplaceRoyalties[_tokenID] > _newBasisPoints, "ERC721::changeMarketplaceRoyalties:Royalties can only be lowered!");
 
+        // set royalties
         marketplaceRoyalties[_tokenID] = _newBasisPoints;
+
+        emit changedMarketplaceRoyalties(_tokenID, _newBasisPoints);
     }
 
-    /* 
-     @dev returns the creator of an NFT for the marketplace contract
-     @param _tokenID 
+
+    /*
+    VIEW FUNCTIONS 
+    ----------------------
+    These functions are used by other contracts to get data on certain
+    parameters such as the creator of a token or the royalties.
     */
+
     function getCreatorOfToken(uint _tokenID) public view returns (address){
         return(creatorOfToken[_tokenID]);
     }
+
+    function getRoyaltiesOfToken(uint _tokenID) public view returns (uint) {
+        return(marketplaceRoyalties[_tokenID]);
+    }
+
+    /*
+    ADMISSION COUNCIL FUNCTIONS
+    -----------------
+    These functions can only be called by the addmissions council. 
+    For Managing permission. 
+    */
+
+    address addmissionCouncilDAO;
+    bool addmissionsCouncilPaused;
+
+    function permitMinters(address[] memory _permitedMinters) external {
+        require(msg.sender == addmissionCouncilDAO, "ERC721::permitMinters:Not authorized.");
+        require(addmissionsCouncilPaused != true, "ERC721::permitMinters:Addmissions Council Paused");
+        
+        for(uint i; i < _permitedMinters.length; i++) {
+            approvedAddresses[_permitedMinters[i]] = true;
+        }
+    }
+
+    function removePermitedMinters(address[] memory _targetAddresses) external {
+        require(msg.sender == addmissionCouncilDAO, "ERC721::removePermitedMinters:Not authorized.");
+        require(addmissionsCouncilPaused != true, "ERC721::removePermitedMinters:Addmissions Council Paused");
+
+        for(uint i; i < _targetAddresses.length; i++) {
+            approvedAddresses[_targetAddresses[i]] = false;
+        }
+    }
+
+    /* 
+    GOVERNANCE FUNCTIONS:
+    ------------------------
+    These functions can be called by the governance contract to change paramters.
+    All onlyOwner functions. Ownership will be transfered to the governance Contract.
+
+    To do:
+        - changePermissionedMinting
+        - add/ remove artists
+        - change Royaltie Paramters (max, changable)
+
+    */
+
+    function changePermissionedMinting(bool _targetValue) external onlyOwner {
+        permissionedMinting = _targetValue;
+    }
+
+    function changeMaxMarketplaceRoyalties(uint _newMaxRoyalties) external onlyOwner {
+        _maxRoyalties = _newMaxRoyalties;
+    }
+
+    function changeAddmissionCouncilStatus(bool _targetValue) external onlyOwner {
+        addmissionsCouncilPaused = _targetValue;
+    }
+
+    // POLICING FUNCTIONS
+    // these are used to give the DAO the ability to regulate the minted tokens
+    // only to be used as a last resort and in an emergency.
+    // not effective or effecient because of the proposal process
+    
+    // burns a token
+    function burnToken(uint _tokenId) external onlyOwner {
+        _burn(_tokenId);
+    }
+
+    // change the URI of a token
+    function changeUriOfToken(uint _tokenId, string memory _newURI) external onlyOwner {
+        _setTokenURI(_tokenId, _newURI);
+    }
+
+
+
+
+
 }
